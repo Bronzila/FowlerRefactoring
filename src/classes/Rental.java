f758d79eb9d67f25c1f520c25a77cb5d9e5d098d
Bronzila <janis_fix@web.de>
2020-05-13 13:21:43 +0200

    Replacing the Conditional Logic on Price Code with Polymorphism

--- src/classes/Rental.java
+++ src/classes/Rental.java
@@ -1,44 +1,26 @@
 package classes;
 
 public class Rental {
-    private Movie movie;
-    private int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
-    }
-    public int getDaysRented() {
-        return daysRented;
-    }
-    public Movie getMovie() {
-        return movie;
-    }
-	public double getCharge() {
-	    double result = 0;
-	    switch (getMovie().getPriceCode()) {
-	        case Movie.REGULAR:
-	            result += 2;
-	            if (getDaysRented() > 2)
-	                result += (getDaysRented() - 2) * 1.5;
-	            break;
-	        case Movie.NEW_RELEASE:
-	            result += getDaysRented() * 3;
-	            break;
-	        case Movie.CHILDRENS:
-	            result += 1.5;
-	            if (getDaysRented() > 3)
-	                result += (getDaysRented() - 3) * 1.5;
-	            break;
-	    }
-	    return result;
+	private Movie movie;
+	private int daysRented;
+
+	public Rental(Movie newmovie, int newdaysRented) {
+		movie = newmovie;
+		daysRented = newdaysRented;
 	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
 	public int getFrequentRenterPoints() {
-		int result = 0;
-        // add frequent renter points
-        result++;
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) 
-            result++;
-        return result;
+		return movie.getFrequentRenterPoints(daysRented);
+	}
+	public double getCharge() {
+		return movie.getCharge(daysRented);
 	}
 }
